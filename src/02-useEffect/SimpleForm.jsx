7120173e211844c95db23d1ac8f0bd7eeab04b81
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-08-29 21:34:57 -0400

    UseEffect Montar y desmontar componente Message

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Message } from './Message'
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'robertbalrog@gmail.com'
+    })
+
+    const {username, email } = formState
+
+    const onInputChange = ({target}) =>{
+        const {name, value} = target
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+  return (
+    <>
+        <h1>Formulario Simple</h1>
+        <hr/>
+
+        <input 
+            type='text'
+            className='form-control'
+            placeholder='Username'
+            name='username'
+            value={username}
+            onChange={onInputChange}
+            
+            />
+        <input 
+            type='email'
+            className='form-control mt-2'
+            placeholder='robert@gmail.com'
+            name='email'
+            value={email}
+            onChange={onInputChange}
+
+
+        />
+        {
+            (username==='strider2') && <Message />
+        }
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import { createRoot } from 'react-dom/client'
 
 // import { HooksApp } from './HooksApp.jsx'
 // import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import './index.css'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterWithCustomHook />
+    {/* <CounterWithCustomHook /> */}
+    <SimpleForm />
   </StrictMode>,
 )
