84a54a5774ebd2874bc68f6336ce569830668c3c
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-02 21:15:39 -0400

    MemoHook useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import { createRoot } from 'react-dom/client'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Memorize } from './06-memos/Memorize'
 
 
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { MemoHook } from './06-memos/MemoHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +21,8 @@ createRoot(document.getElementById('root')).render(
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
     {/* <MultipleCustomHook /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    {/* <Memorize /> */}
+    <MemoHook />
   </StrictMode>,
 )
